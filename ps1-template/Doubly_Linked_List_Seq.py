90a24a6cdb2dc1dcdbf627a9d42c8f8257a46d58
jtanquil <jose.tanquiilut@gmail.com>
2025-09-02 11:51:48 +0800

    fixed ps0 started ps1

--- ps1-template/Doubly_Linked_List_Seq.py
+++ ps1-template/Doubly_Linked_List_Seq.py
@@ -39,19 +39,54 @@ class Doubly_Linked_List_Seq:
         ###########################
         # Part (a): Implement me! #
         ###########################
-        pass
+        # if first element, set the head and tail to the node
+        # otherwise, set self.head = new_head, old_head.prev = new_head, new_head.next = old_head
+        new_head = Doubly_Linked_List_Node(x)
+
+        if self.head is None and self.tail is None:
+            self.head = new_head
+            self.tail = new_head
+        else:
+            old_head, self.head = self.head, new_head
+            new_head.next, old_head.prev = old_head, new_head
 
     def insert_last(self, x):
         ###########################
         # Part (a): Implement me! #
         ###########################
-        pass
+        # if first element, set the head and tail to the node
+        # otherwise, set self.tail = new_tail, old_tail.next = new_tail, new_tail.prev = old_tail
+        new_tail = Doubly_Linked_List_Node(x)
+
+        if self.head is None and self.tail is None:
+            self.head = new_tail
+            self.tail = new_tail
+        else:
+            old_tail, self.tail = self.tail, new_tail
+            new_tail.prev, old_tail.next = old_tail, new_tail
 
     def delete_first(self):
         x = None
         ###########################
         # Part (a): Implement me! #
         ###########################
+        # if there's one element, set both head and tail to None
+        # otherwise, set self.head.next to be the new head
+        if self.head is None:
+            return x
+        else:
+            old_head = self.head
+            x = old_head.item
+
+            if self.head == self.tail:
+                self.head = None
+                self.tail = None
+            else:
+                new_head = old_head.next
+                new_head.prev = None
+                self.head = new_head
+
+            del old_head
         return x
 
     def delete_last(self):
@@ -59,6 +94,23 @@ class Doubly_Linked_List_Seq:
         ###########################
         # Part (a): Implement me! #
         ###########################
+        # if there's one element, set both head and tail to None
+        # otherwise, set self.tail.prev to be the new tail
+        if self.tail is None:
+            return x
+        else:
+            old_tail = self.tail
+            x = old_tail.item
+
+            if self.head == self.tail:
+                self.head = None
+                self.tail = None
+            else:
+                new_tail = old_tail.prev
+                new_tail.next = None
+                self.head = new_tail
+
+            del old_tail
         return x
 
     def remove(self, x1, x2):
